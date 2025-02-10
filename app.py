6484aabe11c620f4d4f719e3190797ee4c08abe9
JBratvold <mcbratjo@gmail.com>
2025-02-09 23:23:33 -0600

    Add catch route for / and /traffic-data

--- app.py
+++ app.py
@@ -1,19 +1,29 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request,  jsonify
 from ai.TrafficController_ai import queryNN
 
 app = Flask(__name__)
 
+
 @app.route("/", methods=["GET", "POST"])
 def index():
-    prediction = None
-    if request.method == "POST":
-        numRedCars = int(request.form["red_cars"])
-        numGreenCars = int(request.form["green_cars"])
-        timeSinceChanged = int(request.form["time_since_changed"])
-        
-        prediction = queryNN(numRedCars, numGreenCars, timeSinceChanged)
-        print("Prediction: ",prediction)
-    return render_template("index.html", prediction=prediction)
+    return render_template("index.html")
+
+# Route to accept traffic data from JS (for sending predictions)
+@app.route('/traffic_data', methods=['POST'])
+def receive_traffic_data():
+    # Get the JSON data sent by JS
+    data = request.get_json()
+
+    # Extract the data from the JSON object
+    redCars = data.get("red", 0)  # Number of cars at a red light
+    greenCars = data.get("green", 0)  # Number of cars at a green light
+    timeSinceChanged = data.get("seconds", 0)  # Time since the light changed
+
+    # Query the neural network with the received data
+    prediction = queryNN(redCars, greenCars, timeSinceChanged)
+    print(prediction)
+    # Return the prediction as a JSON response
+    return jsonify({"prediction": prediction})
 
 if __name__ == "__main__":
     app.run(debug=True)
